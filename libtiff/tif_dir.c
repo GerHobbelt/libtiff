c198eb442119eae94708bac3dceaab0c00812016
Su_Laus <sulau@freenet.de>
2018-10-02 20:51:12 +0200

    EXIF 2.31 and GPS tags and functionality upgraded.
    - Existing EXIF field definition of tags is upgraded to EXIF version 2.3.1
    - EXIF-GPS structure, tags and access functions are added as special CustomDirectory (like it was done for EXIF).
    - Test program custom_dir_EXIF_231.c added to test writing/reading of EXID IFD and GPS IFD tags
      and to highlight some quirks of IFD-handling and peculiarities of reading/writing the different data types.
    - Reading error for FileSource and SceneType tags corrected.
    
    Amended files tif_dirinfo.c, tif_dirread.c, tiff.h, tiffio.h, tif_dir.h, tif_dir.c

--- libtiff/tif_dir.c
+++ libtiff/tif_dir.c
@@ -1403,6 +1403,18 @@ TIFFCreateEXIFDirectory(TIFF* tif)
 	return TIFFCreateCustomDirectory(tif, exifFieldArray);
 }
 
+//--: EXIF-GPS added:
+/*
+ * Creates the GPS custom directory 
+ */
+int
+TIFFCreateGPSDirectory(TIFF* tif)
+{
+	const TIFFFieldArray* gpsFieldArray;
+	gpsFieldArray = _TIFFGetGpsFields();
+	return TIFFCreateCustomDirectory(tif, gpsFieldArray);
+}
+
 /*
  * Setup a default directory structure.
  */
