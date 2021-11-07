50e37dadd5b28d98ac80c1c952226a06c5586dca
Ger Hobbelt <ger@hobbelt.com>
2021-11-08 00:47:54 +0100

    revert AJPG fix: now it's compatible with regular libjpeg and libjpeg-turbo once again.

--- libtiff/tif_jpeg.c
+++ libtiff/tif_jpeg.c
@@ -348,7 +348,7 @@ static int TIFFjpeg_write_raw_data(JPEGState *sp, JSAMPIMAGE data,
                                    int num_lines) {
   return CALLJPEG(
       sp, -1,
-      (int)jpeg_write_raw_data(&sp->cinfo.c, data, (JDIMENSION)num_lines, NULL));
+      (int)jpeg_write_raw_data(&sp->cinfo.c, data, (JDIMENSION)num_lines));
 }
 
 static int TIFFjpeg_finish_compress(JPEGState *sp) {
