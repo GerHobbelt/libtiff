baf4830c86992d4b4c9e89d0e6dcf2faad7b1c5a
Su Laus <sulau@freenet.de>
2025-01-01 19:49:12 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Even Rouault <even.rouault@spatialys.com>

--- libtiff/tif_fax3.c
+++ libtiff/tif_fax3.c
@@ -535,12 +535,13 @@ static int Fax3SetupState(TIFF *tif)
                       "Bits/sample must be 1 for Group 3/4 encoding/decoding");
         return (0);
     }
-    if (td->td_samplesperpixel != 1)
+    if (td->td_samplesperpixel != 1 && td->td_planarconfig != PLANARCONFIG_SEPARATE)
     {
-        TIFFWarningExtR(
+        TIFFErrorExtR(
             tif, module,
-            "Samples/pixel shall be 1 for Group 3/4 encoding/decoding. "
-            "Probably wrong encoding/decoding result");
+            "Samples/pixel shall be 1 for Group 3/4 encoding/decoding, "
+            "or PlanarConfiguration must be set to Separate.");
+        return 0;
     }
     /*
      * Calculate the scanline/tile widths.
